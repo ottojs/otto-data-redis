73e56ff7bfe1a2f40e71ad7b1285ce21daeb7858
Ryan Lelek <ryanlelek@gmail.com>
2014-08-05 14:44:23 -0700

    Adding file connection.js

---
+++ test/connection.test.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+// Modules
+require('should');
+
+// Subject
+var redis_connection = require('../lib/connection.js');
+
+describe('Redis Connection', function () {
+
+  it('should return a Redis Client object', function () {
+    var result = redis_connection();
+    result.should.have.property('set').and.be.type('function');
+    result.should.have.property('get').and.be.type('function');
+  });
+
+});
