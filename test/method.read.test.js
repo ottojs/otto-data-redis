5d38d0750284981d47b78459621d1ba3d33c81db
Ryan Lelek <ryanlelek@gmail.com>
2014-08-07 11:50:31 -0700

    Method .read() now returns JSON strings as an object

--- test/method.read.test.js
+++ test/method.read.test.js
@@ -52,4 +52,64 @@ describe('.read()', function () {
 
   });
 
+  it('should gracefully handle errors', function () {
+    // Generate
+    var read = method_read({
+      get : function (key, next) {
+        next(new Error('Some Error'));
+      }
+    });
+    read('foo', function (error) {
+      error.should.have.property('name').and.equal('Error');
+      error.should.have.property('message').and.equal('Some Error');
+    });
+  });
+
+  it('should leave value of type string untouched', function () {
+
+    // Generate
+    var read = method_read({
+      get : function (key, next) {
+        next(undefined, 'im-a-string!');
+      }
+    });
+    read('foo', function (error, result) {
+      result.should.be.type('string').and.equal('im-a-string!');
+    });
+
+  });
+
+  it('should convert a valid JSON string to an object', function () {
+
+    // Generate
+    var read = method_read({
+      get : function (key, next) {
+        next(undefined, '{"json":"object","with":{"nested":"values"}}');
+      }
+    });
+    read('foo', function (error, result) {
+      result.should.be.type('object').and.eql({
+        json : 'object',
+        with : {
+          nested :'values'
+        }
+      });
+    });
+
+  });
+
+  it('should return value of type string when JSON parsing fails', function () {
+
+    // Generate
+    var read = method_read({
+      get : function (key, next) {
+        next(undefined, '{ this is not JSON!');
+      }
+    });
+    read('foo', function (error, result) {
+      result.should.be.type('string').and.eql('{ this is not JSON!');
+    });
+
+  });
+
 });
