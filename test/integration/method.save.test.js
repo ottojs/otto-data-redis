3ce583323058054c001e9820d01022996d882379
Ryan Lelek <ryanlelek@gmail.com>
2014-11-10 15:46:55 -0800

    Updating method .save() to allow for setting a TTL on the key/value

--- test/integration/method.save.test.js
+++ test/integration/method.save.test.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // Modules
-require('should');
+var should     = require('should');
 var connection = require('../../lib/connection.js');
 var redis      = require('../../lib/index.js');
 
@@ -11,11 +11,37 @@ var subject = require('../../lib/method.save.js')(connection);
 
 describe('.save()', function () {
 
-  it('should successfully save a key/value pair', function (done) {
+  afterEach(function (done) {
+    redis.delete('otto-test-save', done);
+  });
+
+  it('should successfully save a key/value pair without expiration', function (done) {
     subject('otto-test-save', 'save-me', function (error, result) {
-      (error === null).should.equal(true);
+      should.not.exist(error);
       result.should.equal('OK');
-      redis.delete('otto-test-save', done);
+
+      // Check TTL
+      redis.ttl('otto-test-save', function (error, ttl) {
+        should.not.exist(error);
+        ttl.should.equal(-1);
+        done();
+      });
+
+    });
+  });
+
+  it('should successfully save a key/value pair with expiration', function (done) {
+    subject('otto-test-save', 'save-me', 5050, function (error, result) {
+      should.not.exist(error);
+      result.should.equal('OK');
+
+      // Check TTL
+      redis.ttl('otto-test-save', function (error, ttl) {
+        should.not.exist(error);
+        ttl.should.be.above(5000).and.be.below(5100);
+        done();
+      });
+
     });
   });
 
