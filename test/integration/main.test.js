15b5866c9b45257442bd31569deba4b97a67c974
Ryan Lelek <ryanlelek@gmail.com>
2015-03-07 07:56:30 -0800

    Exports: Adding client to exported object

--- test/integration/main.test.js
+++ test/integration/main.test.js
@@ -9,6 +9,10 @@ var subject = require('../../lib/index.js');
 
 describe('Module', function () {
 
+  it('should have property .client', function () {
+    subject.should.have.property('client').and.be.type('object');
+  });
+
   it('should have method .tail()', function () {
     subject.should.have.property('tail').and.be.type('function');
   });
